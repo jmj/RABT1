17bb1cd77670a1c49854f0d1bbc6136ab37240d5
jmj <jeremyj@letifer.org>
2011-07-09 19:10:24 -0500

    Initial code

---
+++ src/rabt1/Invoice.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rabt1;
+
+/**
+ *
+ * @author jjones
+ */
+public class Invoice {
+    private static int numberOfInvoices = 0;
+    
+    private String companyName;
+    private double amountDue;
+    private String chargeDate;
+    private int invoiceNumber;
+    
+    public Invoice() {
+        invoiceNumber = Invoice.numberOfInvoices;
+    }
+    
+    public String getComapanyName() {
+        return companyName;
+    }
+    
+    public double getAmountDue() {
+        return amountDue;
+    }
+    
+    public String getChargeDate() {
+        return chargeDate;
+    }
+    
+    public int getInvoiceNumber() {
+        return invoiceNumber;
+    }
+    
+    public void setCompanyName(String name) {
+        companyName = name;
+    }
+    
+    public void setAmountDue(double amount) {
+        amountDue = amount;
+    }
+    
+    public void setChargeDate(String date) {
+        chargeDate = date;
+    }
+    
+}
