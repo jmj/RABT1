2b190fbfe24d39f8cea01495c0eb2e458c44e4da
jmj <jeremyj@letifer.org>
2011-07-09 20:36:45 -0500

    Added output to verify correctness

--- src/rabt1/RABT1.java
+++ src/rabt1/RABT1.java
@@ -14,10 +14,26 @@ public class RABT1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Invoice invoice3 = new Invoice("Amazing Software", 5000, "January 18, 2009");
-        Invoice invoice1 = new Invoice("Best Programs", 4000, "Feburary 18, 1009");
-        Invoice invoice2 = new Invoice("Champion Code", 3000, "March 18, 2009");
+        Invoice invoice1 = new Invoice("Amazing Software", 5000, "January 18, 2009");
+        Invoice invoice2 = new Invoice("Best Programs", 4000, "Feburary 18, 1009");
+        Invoice invoice3 = new Invoice("Champion Code", 3000, "March 18, 2009");
         
+        System.out.println("Invoice No. " + invoice1.getInvoiceNumber());
+        System.out.println(invoice1.getComapanyName());
+        System.out.format("$%.2f\n", invoice1.getAmountDue());
+        System.out.println(invoice1.getChargeDate());
         
+        System.out.println("\nInvoice No. " + invoice2.getInvoiceNumber());        
+        System.out.println(invoice2.getComapanyName());
+        System.out.format("$%.2f\n", invoice2.getAmountDue());
+        System.out.println(invoice2.getChargeDate());
+
+        System.out.println("\nInvoice No. " + invoice3.getInvoiceNumber());        
+        System.out.println(invoice3.getComapanyName());
+        System.out.format("$%.2f\n", invoice3.getAmountDue());
+        System.out.println(invoice3.getChargeDate());
+        
+        System.out.println("\nInvoice count: " + Invoice.getNumberOfInvoices());
+
     }
 }
